ae15dcc710b1c330fc7da8b97f3d3f063b079e48
JasonNguyen2109 <nguyen.2109@osu.edu>
2023-08-31 20:37:44 -0400

    It works but I did not follow instructions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -22,4 +22,10 @@ function App() {
   );
 }
 
-export default App;
+function HelloWorld() {
+  return (
+<h1>Hello World!</h1>
+  );
+}
+
+export default HelloWorld;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
+import HelloWorld from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+// ReactDOM.render(element, document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld/>
   </React.StrictMode>
 );
 
